59065ebae9e70ffc3adae7a4657e724bd56f4642
Oleksandr_Zhuravlov <zhuravlov.oleksandr@airslate.com>
2023-04-11 19:28:21 +0300

    Exercises from step 2

---
+++ src/JavaBasicSyntax/Step2Task2.java
@@ -0,0 +1,11 @@
+package JavaBasicSyntax;
+
+public class Step2Task2 {
+    public static void main(String[] args) {
+        int a = 12;
+        System.out.println("Post increment - current value: " + a + ", used value: " + a++ + " " + "final value: " + a);
+        System.out.println("Pre increment - current value: " + a + ", used value: " + ++a + " " + "final value: " + a);
+        System.out.println("Post decrement - current value: " + a + ", used value: " + a-- + " " + "final value: " + a);
+        System.out.println("Pre decrement - current value: " + a + ", used value: " + --a + " " + "final value: " + a);
+    }
+}

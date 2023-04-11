bebfa1ffdc30769735ac97036c131412204e760d
Oleksandr_Zhuravlov <zhuravlov.oleksandr@airslate.com>
2023-04-11 20:03:51 +0300

    fix Step3Task1

--- src/JavaBasicSyntax/Step3Task1.java
+++ src/JavaBasicSyntax/Step3Task1.java
@@ -12,8 +12,11 @@ public class Step3Task1 {
         if (a>10){
             System.out.println("Number greater than 10");
         }
-        else if (a<10) {
+        else if (a<10 && a!=0) {
             System.out.println("Number less than 10");
+        }
+        else if (a==0){
+            System.out.println("Number is 0");
         }
         else {
             System.out.println("Number is 10");

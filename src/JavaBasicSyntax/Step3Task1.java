9e4d3ecd44041da0b1b19b08c61aa6d6441925e3
Oleksandr_Zhuravlov <zhuravlov.oleksandr@airslate.com>
2023-04-11 19:33:28 +0300

    Exercises from step 3

---
+++ src/JavaBasicSyntax/Step3Task1.java
@@ -0,0 +1,22 @@
+package JavaBasicSyntax;
+
+import java.util.Scanner;
+
+public class Step3Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Type any number: ");
+        int a = scanner.nextInt();
+
+        if (a>10){
+            System.out.println("Number greater than 10");
+        }
+        else if (a<10) {
+            System.out.println("Number less than 10");
+        }
+        else {
+            System.out.println("Number is 10");
+        }
+    }
+}
---
+++ src/JavaBasicSyntax/Step3Task2.java
@@ -0,0 +1,19 @@
+package JavaBasicSyntax;
+
+import java.util.Scanner;
+
+public class Step3Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Type any number: ");
+        double a = scanner.nextDouble();
+
+        if (a % 2 == 0){
+            System.out.println("Number is even");
+        }
+        else {
+            System.out.println("Number is odd");
+        }
+    }
+}

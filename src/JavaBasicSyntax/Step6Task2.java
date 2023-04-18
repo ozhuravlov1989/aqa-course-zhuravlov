62da9ac2c23ece02305aa3e22c542bad9eba77b9
Oleksandr_Zhuravlov <zhuravlov.oleksandr@airslate.com>
2023-04-19 00:24:58 +0300

    fix step 6 tasks

--- src/JavaBasicSyntax/Step6Task2.java
+++ src/JavaBasicSyntax/Step6Task2.java
@@ -1,11 +1,14 @@
 package JavaBasicSyntax;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Step6Task2 {
 
     public static void main(String[] args) {
-        String str = "Lorem ipsum is java simply dummy text of the printing and typesetting industry";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Type any sentence: ");
+        String str = scanner.nextLine();
         String reversed = "";
         String[] uppercase = str.split(" ");
 

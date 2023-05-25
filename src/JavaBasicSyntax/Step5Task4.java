de2b6dd2bc4212cd8bd47928292d244250d078ef
Oleksandr_Zhuravlov <zhuravlov.oleksandr@airslate.com>
2023-05-25 14:01:30 +0300

    fix chapter 5 tasks

--- src/JavaBasicSyntax/Step5Task4.java
+++ src/JavaBasicSyntax/Step5Task4.java
@@ -5,8 +5,18 @@ import java.util.Arrays;
 public class Step5Task4 {
 
     public static void main(String[] args) {
-        int[] array = {7, 3, 5, 1};
-        Arrays.sort(array);
+        int[] array = {4, 23, 55, 3, 1, 122};
+
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    int k = array[i];
+                    array[i] = array[j];
+                    array[j] = k;
+                }
+            }
+        }
+
         System.out.println(Arrays.toString(array));
     }
 }
